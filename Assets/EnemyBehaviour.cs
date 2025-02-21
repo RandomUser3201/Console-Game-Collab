ee2cc42c2abab3b2818dbf6f6af8364c35e00583
Zeeshan <Mohammed.Zeeshan3@mail.bcu.ac.uk>
2025-02-21 16:11:49 +0000

    Multiple Enemies Chase Player (NavMesh), Added Enemy Spawn Limit & Obstacle Avoidance
    All working well.

---
+++ Assets/EnemyBehaviour.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public Transform player;
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.position);
+    }
+}
--- Assets/EnemySpawner.cs
+++ Assets/EnemySpawner.cs
@@ -9,6 +9,9 @@ public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
     public GameObject cube;
+
+    private int enemiesSpawned;
+
     [SerializeField] private float minX = 2, maxX = 6;
     [SerializeField] private float minZ = 2, maxZ = 6;
     [SerializeField] private float spawnCooldown = 5f;
@@ -29,12 +32,21 @@ public class EnemySpawner : MonoBehaviour
 
     public void SpawnEnemy()
     {
+        if (enemiesSpawned == 10)
+        {
+            Debug.LogWarning("Enemy Limit REACHED!!!: " + enemiesSpawned);
+            return;
+        }
+
         float randomX = Random.Range(minX, maxX);
         float randomZ = Random.Range(minZ, maxZ);
 
         Vector3 randomPosition = new Vector3(randomX, spawnHeight, randomZ);
 
         Instantiate(enemyPrefab, randomPosition, Quaternion.identity);
+
+        enemiesSpawned++;
+        Debug.LogWarning("Enemy Limit: " + enemiesSpawned);
     }
 
     private void OnCollisionEnter(Collision other)
