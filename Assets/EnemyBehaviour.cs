a22c211eb1136a834b208fd04b5355b9211d0575
Zeesh <Mohammed.Zeeshan3@mail.bcu.ac.uk>
2025-02-22 23:51:33 +0000

    Health Displayed GUI Above Enemy Head

--- Assets/EnemyBehaviour.cs
+++ Assets/EnemyBehaviour.cs
@@ -2,12 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-
+using UnityEngine.UI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
     public Transform player;
     public NavMeshAgent agent;
+
+
+    // Enemy Health Manager
+    public float health = 100f;
+    public float maxHealth = 100f;
+    private Text healthText;
+    public GameObject healthDisplayPrefab;
+    private GameObject healthDisplay;
+
 
     // Start is called before the first frame update
     void Start()
@@ -15,11 +24,34 @@ public class EnemyBehaviour : MonoBehaviour
         player = GameObject.FindWithTag("Player").transform;
 
         agent = GetComponent<NavMeshAgent>();
+
+        healthDisplay = Instantiate(healthDisplayPrefab, transform);
+        healthDisplay.transform.localPosition = new Vector3(0, 2, 0);
+
+
+        healthText = healthDisplay.GetComponentInChildren<Text>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
         agent.SetDestination(player.position);
+
+        healthText.text = Mathf.RoundToInt(health).ToString();
+        transform.LookAt(Camera.main.transform); 
     }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        if (health == 0)
+        {
+            Debug.LogWarning("Health is now 0: " + health);
+        }
+    }
+
+    // public float HealthPercentage()
+    // {
+    //     return health / maxHealth;
+    // }
 }
